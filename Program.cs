952132ec9c1559ec996e4204e9c0dedd7e66c944
Сергей Усков <q3mydoom@hotmail.com>
2021-10-25 14:30:48 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Homework_1._4_Task_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
